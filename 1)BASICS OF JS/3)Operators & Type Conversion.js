ae244f15a0aeb57b3c5a63b71ee2af5d03729133
Kuyasha Das <43484992+Kuyasha@users.noreply.github.com>
2022-03-11 03:42:21 +0530

    Add files via upload

---
+++ 1)BASICS OF JS/3)Operators & Type Conversion.js
@@ -0,0 +1,30 @@
+//ARITHMETIC OPERATOR
+console.log(10+2);
+console.log(13/2);
+console.log(0/0); //output=NaN
+console.log(1/0); //Infinity
+console.log(Infinity*0);//NaN
+console.log(2*3);
+console.log(13%NaN);//NaN
+console.log(13%Infinity);//NaN
+
+//ASSIGNMENT OPARATORS
+var a=10;
+console.log(a+=2);
+console.log(a*=2);
+
+//UNARY OPERATOR
+console.log(a++);
+console.log(++a);
+
+//TYPE CONVERSION
+console.log("hello" + "world");
+console.log("hello" + 2);
+console.log("1" + 1);
+console.log('1' - 1);
+console.log('1' * 5);
+console.log('1'/2);
+console.log('1'>0);
+
+
+
