42132d5fc0b011fdf64032da72a2c8d038fdabe6
Kuyasha Das <43484992+Kuyasha@users.noreply.github.com>
2022-03-11 03:40:42 +0530

    Add files via upload

---
+++ 1)BASICS OF JS/1)Varibles,Undefined,Null.js
@@ -0,0 +1,19 @@
+//JS variable is not bound to a type but Value is bound to a type.In one line if we define var as integer,
+//on the next line we can define it as string.
+var a=10;
+console.log(a);
+
+a="hello";
+console.log(a);
+
+//UNDEFINED
+var b;
+console.log(b);
+
+//NULL (datatype of null=object)
+var address = null;
+console.log(address);
+
+var a="A";
+console.log(65-a);
+
