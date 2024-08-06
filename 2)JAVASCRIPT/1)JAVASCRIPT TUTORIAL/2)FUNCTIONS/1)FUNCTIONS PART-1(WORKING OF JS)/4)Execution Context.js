43bba2ccd7d97c4c607ed01560dbf31024e29405
Kuyasha Das <43484992+Kuyasha@users.noreply.github.com>
2024-08-06 08:50:58 +0530

    Add files via upload

---
+++ 2)JAVASCRIPT/1)JAVASCRIPT TUTORIAL/2)FUNCTIONS/1)FUNCTIONS PART-1(WORKING OF JS)/4)Execution Context.js
@@ -0,0 +1,15 @@
+var userName='Tom';
+var userAge=10;
+console.log(`username:${userName}`);
+console.log(`userage:${userAge}`);
+
+function greetUser(name){
+    var greet='I hope you are doing fine.';
+    console.log(`hello ${name}, ${greet}`);
+    var currentYear=2030;
+    const year=currentYear-userAge;
+    return `Your birthyear is ${year}`;
+}
+
+const birthYear=greetUser(userName);
+console.log(birthYear);
