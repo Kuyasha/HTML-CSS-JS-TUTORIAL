c6b106b20a446c7914602415177998d488abc6c2
Kuyasha Das <43484992+Kuyasha@users.noreply.github.com>
2024-08-06 08:57:39 +0530

    Add files via upload

---
+++ 2)JAVASCRIPT/1)JAVASCRIPT TUTORIAL/2)FUNCTIONS/1)FUNCTIONS PART-1(WORKING OF JS)/11)strict mode.js
@@ -0,0 +1,30 @@
+/*
+//1)a)
+let value=10;
+if(value){
+    valeu=20;
+}
+console.log(value)
+//op=>10;
+*/
+
+//1)b)
+'use strict';
+let value=10;
+if(value)
+{
+    valeu=20;//spelling mistake
+}
+console.log(value);
+//op=>ReferenceError: valeu is not defined
+
+
+
+
+//2)As we have used strict mode;we need to mention the datatype of a.
+'use strict';
+x=10;
+console.log(x);
+//op=> ReferenceError:x is not defined
+
+
