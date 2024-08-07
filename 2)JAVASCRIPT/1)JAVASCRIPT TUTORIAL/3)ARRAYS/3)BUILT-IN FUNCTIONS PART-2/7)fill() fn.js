487681feddcf5789704ab648df278c7ba02b9871
Kuyasha Das <43484992+Kuyasha@users.noreply.github.com>
2024-08-08 03:27:05 +0530

    Add files via upload

---
+++ 2)JAVASCRIPT/1)JAVASCRIPT TUTORIAL/3)ARRAYS/3)BUILT-IN FUNCTIONS PART-2/7)fill() fn.js
@@ -0,0 +1,9 @@
+
+// fill=>
+//The fill function changes all elements in an array with a static value,
+//starting from a specified start index and ending at a specified end index.
+
+const numbers = [1, 2, 3, 4, 5,6];
+numbers.fill(0, 2, 4);
+console.log(numbers);
+ // Outputs: [1, 2, 0, 0, 5]
