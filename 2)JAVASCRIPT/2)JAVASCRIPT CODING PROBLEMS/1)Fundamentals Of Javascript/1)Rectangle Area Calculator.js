a0d1e7dd9791d239ba333ac65ac4591fe9c686d9
Kuyasha Das <43484992+Kuyasha@users.noreply.github.com>
2024-08-09 08:04:48 +0530

    Add files via upload

---
+++ 2)JAVASCRIPT/2)JAVASCRIPT CODING PROBLEMS/1)Fundamentals Of Javascript/1)Rectangle Area Calculator.js
@@ -0,0 +1,16 @@
+/*
+PROBLEM STATEMENT: Write a program to calculate the area of the rectangle
+for the given dimension (len and wid). Store the area in the result variable.
+
+Sample Input:5, 5
+Expected Output:25
+
+*/
+
+function main(length,width){
+    let area = length * width;
+    result = area;
+    return result;
+}
+
+console.log(main(5,5));
